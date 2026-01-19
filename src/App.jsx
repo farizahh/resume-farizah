3c100f9d29cb28f683570613d29e19e0016e82d6
farizahh <farhanafarizah@gmail.com>
2026-01-19 10:06:46 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Coba from './pages/Coba'
+
+const App = () => {
+  return (
+    <>
+      <Coba />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Coba.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Test from '../component/Test'
+
+const Coba = () => {
+    return (
+        <div>
+            <Test />
+        </div>
+    )
+}
+
+export default Coba
